5da73cb82256b8bf95e6a7f9723fd28be84bd342
Diamssword <hugo.piechowiak@gmail.com>
2018-12-06 22:25:06 +0100

    Create mob.js

---
+++ JavaScript/class/mob.js
@@ -0,0 +1,33 @@
+class Mob{
+  constructor(x,y,width,height,image){
+    this.x = y;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.image = image;
+  }
+
+
+  get width()
+  {
+    return this.width;
+  }
+  get height()
+  {
+    return this.height;
+  }
+  get x()
+  {
+    return this.x;
+  }
+  get y()
+  {
+    return this.y;
+  }
+
+  get image()
+  {
+    return this.image;
+  }
+
+}
