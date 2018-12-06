b42139e767cdb0cce45a1a2e3a82f625e4cf4743
Yann-Thevenet <45660303+Yann-Thevenet@users.noreply.github.com>
2018-12-07 00:22:29 +0100

    blabla

--- JavaScript/class/main.js
+++
@@ -1,3 +0,0 @@
-function test(){
-  var test = new Obstacle(1,1,"test",10,10);
-}
--- JavaScript/class/mob.js
+++ JavaScript/class/mob.js
@@ -1,29 +1,5 @@
-class Mob {
-  constructor(position_x, position_y, hauteur, largeur, image) {
-    this.position_x = position_x;
-    this.position_y = position_y;
-    this.width = largeur;
-    this.hauteur = hauteur;
-    this.image = image;
-  }
-
-  getX() {
-    return this.position_x;
-  }
-
-  getY() {
-    return this.position_y;
-  }
-
-  getWidth() {
-    return this.width;
-  }
-
-  getHeight() {
-    return this.height;
-  }
-
-  getImage() {
-    return this.image;
+class Mob extends Composant {
+  constructor(position_x, position_y, hauteur, largeur, cheminImage) {
+    super(position_x, position_y, hauteur, largeur, cheminImage);
   }
 }
