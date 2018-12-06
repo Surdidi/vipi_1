e6dd4bba78336507d4489b8d2562f477a2a1391f
Dautheville Dylan <surdidi@gmail.com>
2018-12-06 22:45:13 +0100

    aa

---
+++ JavaScript/class/Buisson.js
@@ -0,0 +1,7 @@
+class Buisson extends Obstacle {
+
+    constructor(hauteur,largeurposition_x,position_y){
+      image = "../../img/buisson.png";
+      super(hauteur,largeur,image,position_x,position_y);
+    }
+}
--- JavaScript/class/Cactus.js
+++ JavaScript/class/Cactus.js
@@ -1,6 +1,7 @@
 class Cactus extends Obstacle {
 
-    constructor(hauteur,largeur,image,position_x,position_y){
+    constructor(hauteur,largeur,position_x,position_y){
+      image = "../../img/Cactus.png";
       super(hauteur,largeur,image,position_x,position_y);
     }
 }
