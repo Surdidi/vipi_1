4fccb1d7c1a4b8838c0e2a17e3931a69544cb546
Yann-Thevenet <45660303+Yann-Thevenet@users.noreply.github.com>
2018-12-07 03:01:07 +0100

    class

--- Vipi_javascript_de_merde/src/Class/Composant.java
+++ Vipi_javascript_de_merde/src/Class/Composant.java
@@ -1,5 +1,47 @@
 package Class;
 
-public class Composant {
+import javax.swing.ImageIcon;
 
+public class Composant {
+	private Integer positionX;
+	private Integer positionY;
+	private Integer hauteur;
+	private Integer largeur;
+	private ImageIcon image;
+	
+	public Composant(Integer positionX, Integer positionY, Integer hauteur, Integer largeur, String linkImage) {
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.hauteur = hauteur;
+		this.largeur = largeur;
+		this.image = new ImageIcon(getClass().getResource(linkImage));
+	}
+	
+	public Integer getPositionX() {
+		return positionX;
+	}
+	
+	public void setPositionX(Integer positionX) {
+		this.positionX = positionX;
+	}
+	
+	public Integer getPositionY() {
+		return positionY;
+	}
+	
+	public void setPositionY(Integer positionY) {
+		this.positionY = positionY;
+	}
+	
+	public Integer getHauteur() {
+		return hauteur;
+	}
+	
+	public Integer getLargeur() {
+		return largeur;
+	}
+	
+	public ImageIcon getImage() {
+		return image;
+	}
 }
---
+++ Vipi_javascript_de_merde/src/Class/Vipi.java
@@ -0,0 +1,6 @@
+package Class;
+
+public class Vipi extends Composant {
+	private Integer vie = 121;
+	private Integer 
+}
