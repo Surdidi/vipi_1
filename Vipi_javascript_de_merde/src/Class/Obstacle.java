3877df2d6dd81332e1edc25c420c8a18326e749a
Yann-Thevenet <45660303+Yann-Thevenet@users.noreply.github.com>
2018-12-07 03:37:55 +0100

    java

--- Vipi_javascript_de_merde/src/Class/Obstacle.java
+++ Vipi_javascript_de_merde/src/Class/Obstacle.java
@@ -1,8 +1,10 @@
 package Class;
 
+import java.io.IOException;
+
 public class Obstacle extends Composant {
 	
-	public Obstacle(Integer positionX, Integer positionY, Integer hauteur, Integer largeur,String link) {
+	public Obstacle(Integer positionX, Integer positionY, Integer hauteur, Integer largeur,String link) throws IOException {
 		/*int alea = (int) (Math.random() * ( 1 - 0 )) ;
 		if(alea == 0) {
 			String img = 
