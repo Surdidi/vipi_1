b5f0895d27c7779595ff5013130db0d7571ea54b
Yann-Thevenet <45660303+Yann-Thevenet@users.noreply.github.com>
2018-12-07 03:12:02 +0100

    afazf

---
+++ Vipi_javascript_de_merde/src/Class/Mob.java
@@ -0,0 +1,8 @@
+package Class;
+
+public class Mob extends Composant {
+	
+	public Mob(Integer positionX, Integer positionY) {
+		super(positionX, positionY, 200, 200, "");
+	}
+}
--- Vipi_javascript_de_merde/src/Class/Vipi.java
+++ Vipi_javascript_de_merde/src/Class/Vipi.java
@@ -2,5 +2,47 @@ package Class;
 
 public class Vipi extends Composant {
 	private Integer vie = 121;
-	private Integer 
+	private Integer hydratation = 121;
+	private Integer energie = 121;
+	private boolean ch2019 = true;
+	
+	public Vipi() {
+		super(0, 0, 200, 200, "../images/totem.png");
+	}
+	
+	public Integer getVie() {
+		return vie;
+	}
+	
+	public void setVie(Integer vie) {
+		this.vie = vie;
+	}
+	
+	public Integer getHydratation() {
+		return hydratation;
+	}
+	
+	public void setHydratation(Integer hydratation) {
+		this.hydratation = hydratation;
+	}
+	
+	public Integer getEnergie() {
+		return energie;
+	}
+	
+	public void setEnergie(Integer energie) {
+		this.energie = energie;
+	}
+	
+	public boolean isCh2019() {
+		return ch2019;
+	}
+	
+	public void changeCh2019() {
+		if(this.ch2019 == true) {
+			this.ch2019 = false;
+		} else {
+			this.ch2019 = true;
+		}
+	}
 }
