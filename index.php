0a45594513ec940b2190163b367f565f67752775
Yann-Thevenet <45660303+Yann-Thevenet@users.noreply.github.com>
2018-12-07 02:04:19 +0100

    geerg

--- index.php
+++ index.php
@@ -17,9 +17,6 @@
     <script src="../JavaScript/class/Serpent.js"></script>
     <script langage="javascript">
 
-      var background = new Background(document.documentElement.clientWidth/2,document.documentElement.clientWidth.);
-      document.body.appendChild(background.getImage());
-
       var vipi = new Vipi(0, 0);
       document.body.appendChild(vipi.getImage());
 
