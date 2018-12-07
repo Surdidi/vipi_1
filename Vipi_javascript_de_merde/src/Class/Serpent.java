82f60ecade773ea74171735614cb44449a3077c3
Dautheville Dylan <surdidi@gmail.com>
2018-12-07 03:39:22 +0100

    aa

--- Vipi_javascript_de_merde/src/Class/Serpent.java
+++ Vipi_javascript_de_merde/src/Class/Serpent.java
@@ -3,15 +3,27 @@ package Class;
 public class Serpent extends Mob {
 
 	private int positionInit ;
-	private 
 	
 	public Serpent(Integer positionX, Integer positionY, int positionInit) {
 		super(positionX,positionY);
+		this.positionInit = positionInit;
 	}
 	
 	public void déplacement() {
-		if(positionX < positionInit-100) {
-			
+		int i = 0 ;
+		if(positionX < positionInit) {
+			while(positionX != positionInit + 100) {
+				i++;
+				positionX += 3 ;
+				// a repaint
+			}
+		}
+		else {
+			while(positionX != positionInit - 100) {
+				i++;
+				positionX = positionX - 3 ;
+				// a repaint
+			}
 		}
 	}
 	
