b251110d07666ddcc87a2649e4d1f7ec594849b7
Yann-Thevenet <45660303+Yann-Thevenet@users.noreply.github.com>
2018-12-07 02:35:02 +0100

    sdg

--- index.php
+++ index.php
@@ -1,42 +1,39 @@
 <html>
-<head>
-  <title> index </title>
-  <link rel="stylesheet" href="./assets/style.css">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-</head>
-<body>
-  <script langage="javascript">
-  var partie = new Controlleur();
-  </script>
-  <div class="row">
-    <div class="col" style="background-color:yellow;">25%</div>
-    <div class="col" style="background-color:orange;">25%</div>
-    <div class="col" style="background-color:yellow;">25%</div>
-    <div class="col" style="background-color:orange;">25%</div>
-    <div class="col" style="background-color:orange;">25%</div>
-    <div class="col" style="background-color:orange;">25%</div>
-    <div class="col" style="background-color:orange;">25%</div>
-    <div class="col" style="background-color:orange;">25%</div>
-    <div class="col" style="background-color:orange;">25%</div>
-    <div class="col" style="background-color:orange;">25%</div>
-  </div>
+  <head>
+    <title> index </title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <script src="../JavaScript/class/Composant.js"></script>
+    <script src="../JavaScript/class/Vipi.js"></script>
 
-  <script src="../JavaScript/class/Composant.js"></script>
-  <script src="../JavaScript/class/Vipi.js"></script>
+    <script src="../JavaScript/class/Obstacle.js"></script>
+    <script src="../JavaScript/class/Rocher.js"></script>
+    <script src="../JavaScript/class/Cactus.js"></script>
+    <script src="../JavaScript/class/Buisson.js"></script>
 
-  <script src="../JavaScript/class/Obstacle.js"></script>
-  <script src="../JavaScript/class/Rocher.js"></script>
-  <script src="../JavaScript/class/Cactus.js"></script>
-  <script src="../JavaScript/class/Buisson.js"></script>
+    <script src="../JavaScript/class/Mob.js"></script>
+    <script src="../JavaScript/class/Taupe.js"></script>
+    <script src="../JavaScript/class/Serpent.js"></script>
+    <script langage="javascript">
 
-  <script src="../JavaScript/class/Mob.js"></script>
-  <script src="../JavaScript/class/Taupe.js"></script>
-  <script src="../JavaScript/class/Serpent.js"></script>
-  <script src="../JavaScript/Controlleur.js"></script>
-</body>
+      var vipi = new Vipi(0, 0);
+      document.body.appendChild(vipi.getImage());
+
+      var rocher = new Rocher(1,1);
+      document.body.appendChild(rocher.getImage());
+
+      var cactus = new Cactus(1,1);
+      document.body.appendChild(cactus.getImage());
+
+      var buisson = new Buisson(1,1);
+      document.body.appendChild(buisson.getImage());
+
+      var taupe = new Taupe(1,1);
+      document.body.appendChild(taupe.getImage());
+
+      var serpent = new Serpent(1,1);
+      document.body.appendChild(serpent.getImage());
+    </script>
+  </body>
 </html>
