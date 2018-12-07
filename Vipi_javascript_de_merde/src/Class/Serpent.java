a522b5d509812192133f986145fd2fe5350d38f4
Dautheville Dylan <surdidi@gmail.com>
2018-12-07 03:32:49 +0100

    vv

---
+++ Vipi_javascript_de_merde/src/Class/Serpent.java
@@ -0,0 +1,18 @@
+package Class;
+
+public class Serpent extends Mob {
+
+	private int positionInit ;
+	private 
+	
+	public Serpent(Integer positionX, Integer positionY, int positionInit) {
+		super(positionX,positionY);
+	}
+	
+	public void déplacement() {
+		if(positionX < positionInit-100) {
+			
+		}
+	}
+	
+}
